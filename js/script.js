929eadbfe88c26a793d861f7e844379e462c4ea6
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-05-02 21:30:17 -0300

    Iniciando a criação dos Cartões - desenvolvedo com Grid

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
+// Minha resolução para esconder o menu da pagina 
+// function ativaEdesativaMenu(){
+//     $nav.classList.toggle('menu-lateral__escondido')
+// }
+
+// const $menu = document.getElementsByClassName('container__menu')[0];
+// const $nav = document.getElementsByClassName('menu-lateral')[0];
+
+// $menu.addEventListener('click', ativaEdesativaMenu)
+
+//Resolução da aula
 
 const $menu = document.querySelector('.container__menu');
 const $nav = document.querySelector('.menu-lateral');
 
 $menu.addEventListener('click', () => {
-    $nav.classList.toggle('menu-lateral__ativo')
-});
+    $nav.classList.toggle('menu-lateral__ativo');
+})
+
+
